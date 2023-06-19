c5e8cb366f91c098d7d647a61f8b35becfcb03d2
tatelulove4 <tatelulove4@naver.com>
2023-06-19 13:16:57 +0900

    lunch time

--- src/ForInFor.java
+++ src/ForInFor.java
@@ -1,11 +1,11 @@
+import java.util.Scanner;
 
 public class ForInFor {
 	public static void main(String[] args) {
-		for(int i = 1 ; i < 10 ; i++) {
-			if(i % 2 == 0) continue;
-			for(int j = 1; j < 10 ; j++) {
-				if(j % 2 == 0) continue;
-				System.out.println(i + " x " + j + " = " + i*j);
+		for(int i =1;i<10;i++) {
+			if(i%2 == 1) continue;
+			for(int j =1; j<10;j++) {
+				System.out.println(i + " * " + j + " = " + i*j);
 			}
 			System.out.println();
 		}
