6bcdd27f700d0eeb0a1e78c15ca7a7e4721f6692
geoni4 <seralove4@gmail.com>
2023-07-04 22:22:35 +0900

    nono

--- src/RSPMain.java
+++ src/RSPMain.java
@@ -1,21 +1,45 @@
+import java.util.Scanner;
 
 class RockScissorsPaper{
 	private String[] counterPart = new String[] { "가위", "바위", "보"};
-
+	private String myValue;
+ 
 	
 	
-	public RockScissorsPaper(String[] counterPart) {
-		this.counterPart = counterPart;
+	public RockScissorsPaper() {
 	}
 	
 	
+	public String getCounterPart() {
+		int num = (int) Math.random() * 3;
+		return counterPart[num];
+	}
 	
-	
+	public String getMyValue() {
+		return myValue;
+	}
+
+
+	public void setMyValue(String myValue) {
+		this.myValue = myValue;
+	}
+
+	public String versus() {
+		String counterPartValue = getCounterPart();
+		if(myValue.equals(counterPartValue))
+			return "비겼습니다";
+		
+	}
 }
 
 public class RSPMain {
 	public static void main(String[] args) {
-		int num = (int) Math.random() * 3;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		RockScissorsPaper rsp = new RockScissorsPaper();
+		
+		
 		
 	}
 }
